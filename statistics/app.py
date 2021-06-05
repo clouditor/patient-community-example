a3f75b90e2ab596903fb5c6396f48545e7b07eb2
Kunz, Immanuel <immanuel.kunz@aisec.fraunhofer.de>
2021-06-05 12:36:52 +0200

    get k-anonymized health data

--- statistics/app.py
+++ statistics/app.py
@@ -8,13 +8,10 @@ from flask_jwt_extended import jwt_required
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import create_access_token
 from flask import Flask, request
-from bson import json_util
-import sys
-import logging
 import jwt
 import json
 import requests
-import datetime
+from basic_mondrian_health import health_data_api
 
 client = MongoClient("mongodb://localhost:27017/")
 
@@ -32,19 +29,34 @@ app.config["JWT_PUBLIC_KEY"] = ECAlgorithm.from_jwk(
 
 app.config["JWT_ALGORITHM"] = "ES256"
 
-
 @app.route("/api/v1/statistics", methods=['GET'])
 @jwt_required()
 def list_statistics():
-    user_id = get_jwt_identity()
     claims = get_jwt()
 
-    if claims["scope"] != "researcher":
-        return json.dumps({"error": "Invalid scope"}), 403
+    # if claims["scope"] != "researcher":
+    #     return json.dumps({"error": "Invalid scope"}), 403
 
-    records = collection.find()
+    # if there are not enough records, generate some
+    while collection.count_documents({}) < 4:
+        new_record = health_data_api.generate_record().split(',')
+        collection.insert_one({
+            "group": new_record[0], 
+            "timestamp": new_record[1], 
+            "medication": new_record[2], 
+            "medication_dosage": new_record[3], 
+            "symptom": new_record[4], 
+            "symptom_strength": new_record[5]
+        })
+        
 
-    return json_util.dumps(records), 200
+    cursor = collection.find({}, {"_id": 0, "group": 1, "timestamp": 1, "medication": 1, "medication_dosage": 1, "symptom": 1, "symptom_strength": 1})
+    cursor = list(cursor)
+    
+    anonymized_data = health_data_api.anonymize_mongo_data(cursor)
+
+    return str(anonymized_data), 200
 
 if __name__ == '__main__':
+    # print(list_statistics())
     app.run(host='0.0.0.0', port=8084, debug=True, threaded=True)
