2b2b07b3db4b13b50abd67b2d8f7870f2f4bcaa9
Kunz, Immanuel <immanuel.kunz@aisec.fraunhofer.de>
2021-06-21 14:19:47 +0200

    initial working structure

---
+++ deployments/docker-compose/mongo/initial-mongo-data.js
@@ -0,0 +1,20 @@
+db.records.insertMany([
+  {
+    user_id: "1234",
+    group: "anxiety1",
+    timestamp: "2021-06-03T13:18:56+00:00", 
+    medication: "cardiovascular",
+    medication_dosage: 5, 
+    symptom: "chills",
+    symptom_strength: 4,
+  },
+  {
+    user_id: "1235",
+    group: "anxiety1",
+    timestamp: "2021-06-02T13:18:56+00:00", 
+    medication: "nervous",
+    medication_dosage: 7, 
+    symptom: "dizzy",
+    symptom_strength: 2,
+  }
+]);
