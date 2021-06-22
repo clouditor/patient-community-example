8623bd60de65cff78490a8cc251ee820e57ec951
Kunz, Immanuel <immanuel.kunz@aisec.fraunhofer.de>
2021-06-22 17:55:30 +0200

    rebase+

--- deployments/docker-compose/postgres/init.sql
+++ deployments/docker-compose/postgres/init.sql
@@ -1,9 +1,24 @@
-CREATE TABLE group_memberz
+CREATE TABLE users
 (
-    username TEXT PRIMARY KEY,
+    id INT PRIMARY KEY,
+    username TEXT,
     firstname TEXT,
     lastname TEXT,
     password TEXT,
-    role TEXT
+    role INT
 );
-COPY group_memberz FROM '/data/data.csv' DELIMITER ',' CSV HEADER;
+COPY users FROM '/data/data_users.csv' DELIMITER ',' CSV HEADER;
+
+CREATE TABLE groups
+(
+    id INT PRIMARY KEY,
+    name TEXT
+);
+COPY groups FROM '/data/data_groups.csv' DELIMITER ',' CSV HEADER;
+
+CREATE TABLE group_members
+(
+    group_id INT PRIMARY KEY,
+    user_id INT
+);
+COPY group_members FROM '/data/data_group_members.csv' DELIMITER ',' CSV HEADER;
