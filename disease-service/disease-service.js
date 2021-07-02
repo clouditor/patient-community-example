64565ad0d43ba8a83e699daef9b69498f489c1b2
Konrad Weiss <konrad.weiss13@gmail.com>
2021-07-02 08:18:08 +0200

    Kweiss/disease service (#74)
    * Adding disease service implemented in nodejs with express
    
    * Fixing ports in docker compose
    
    * added docker, k8s
    
    * fixed job id
    
    * fix naming
    
    Co-authored-by: Kunz, Immanuel <immanuel.kunz@aisec.fraunhofer.de>

---
+++ disease-service/disease-service.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const app = express()
+const port = 8086
+
+const path = require('path');
+
+const readline = require('readline');
+
+var fs = require('fs');
+
+let symptomsToDiseases = initSymptomsToDiseasesMap()
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.get('/api/v1/diseases', (req, res) => {
+
+  let symptoms = req.body;
+
+  return res.json(getDiseases(symptoms));
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+function getDiseases(symptoms) {
+  let response = []
+
+  for (symptom of symptoms){
+    let diseases = symptomsToDiseases.get(symptom);
+
+    Array.prototype.push.apply(response, diseases);
+  }
+
+  return [...new Set(response)];
+}
+
+function initSymptomsToDiseasesMap(){
+  let sToDMap = new Map();
+
+  
+  var files = fs.readdirSync('symptomsToDiseases/');
+  
+  for (file of files) {
+    const readInterface = readline.createInterface({
+      input: fs.createReadStream(path.resolve('symptomsToDiseases/', file)),
+      output: process.stdout,
+      console: false
+    });
+    let diseases = []
+    readInterface.on('line', function(line) {
+      diseases.push(line)
+    });
+
+    sToDMap.set(file, diseases)
+  }
+
+  return sToDMap;
+}
+
+
+
+
