568bf04f3f3c23ff11ff584e45d3e1e89d273800
Kunz, Immanuel <immanuel.kunz@aisec.fraunhofer.de>
2023-05-19 10:12:15 +0200

    clean imports, add postgres to env vars

--- group-phr-controller/app.py
+++ group-phr-controller/app.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 
 from jwt.algorithms import ECAlgorithm
-from pymongo import MongoClient, database
+from pymongo import MongoClient
 from flask_jwt_extended import JWTManager
 from flask_jwt_extended import get_jwt
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import get_jwt_identity
-from flask import Flask, request
+from flask import Flask
 from bson import json_util
 import json
 import requests
 import os
 import psycopg2
-from configparser import ConfigParser
 import hashlib
 
 # user_db connection (PostgreSQL)
@@ -46,10 +45,12 @@ app.config["JWT_PUBLIC_KEY"] = ECAlgorithm.from_jwk(
 
 app.config["JWT_ALGORITHM"] = "ES256"
 
-@app.route("/api/v1/groupdata/groupid/<int:group_id>/userid/<int:user_id>", methods=['GET'])
+# TODO psuedonymization configurable?
+
+@app.route("/api/v1/groupdata/<int:group_id>", methods=['GET'])
 @jwt_required()
-def list_groupdata(group_id, user_id):
-    # In a secure application the user id should be taken from the jwt: user_id = get_jwt_identity()
+def list_groupdata(group_id=0):
+    user_id = get_jwt_identity()
     claims = get_jwt()
     # records = []
 
@@ -61,24 +62,22 @@ def list_groupdata(group_id, user_id):
     try:
         group_ids_list = []
         
-        # ID7: the service accesses the user database which contains identifiers
-        user_db.execute("""SELECT group_id FROM group_members WHERE user_id=(%s)""", (str(user_id)))
+        user_db.execute("""SELECT group_id FROM group_members WHERE user_id=(%s)""", (user_id))
         rows = user_db.fetchall()
 
         for row in rows:
             group_ids_list.append(row[0])
 
         # Checks if requesting user has the requested group_id
-        # D5: a user can try to request data for another user/group combination and learn about which user is in which group
         if group_id not in group_ids_list:
-            return json.dumps({"error": "user is not member of the specified group"}), 404
+            return json.dumps({"error": "user has no permission for the requested group_id"}), 403
 
     except Exception as e:
         print("err: ", e)
         return json.dumps({"error": "user_db request"}), 500
 
     
-    # L7: the service accesses both the user DB and the PHR DB and can link the data
+    # Now we can retrieve the group_data
     # Get group_data for requested group_id
     records = phr_db_collection.find({"group_id": group_id})
     
@@ -88,12 +87,10 @@ def list_groupdata(group_id, user_id):
     for item in json_data:
         item["user_id"] = item["user_id"].replace(user_id, hash_user_id(user_id))
 
-    # T4
     return json_util.dumps(json_data), 200
 
 def hash_user_id(user_id):
-    #@Identifier
     return hashlib.md5(user_id.encode()).hexdigest()
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8085, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=8085, debug=True, threaded=True)
